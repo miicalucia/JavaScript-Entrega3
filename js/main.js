68ef8d6dcc570d1b99dc1717ed0a68c755641931
mlg <miicalucia95@gmail.com>
2023-05-10 17:20:13 -0300

    cambio proyecto

--- js/main.js
+++ js/main.js
@@ -1,16 +1,55 @@
 class Tarea {
-    constructor(nombre, fecha, hora, nota) {
+    constructor(nombre, fecha, nota) {
         this.nombre = nombre;
         this.fecha = fecha;
-        this.hora = hora;
         this.nota = nota;
     }
 }
 
 const listaDeTareas = [];
-console.log(listaDeTareas);
 
-function agregarNuevo() {
+let tareasPendientes = [];
+
+if (localStorage.getItem("tareasPendientes")) {
+    tareasPendientes = JSON.parse(localStorage.getItem("tareasPendientes"));
+}
+
+const inputValue = document.getElementById("nombreUsuario");
+inputValue.value = "Ingrese su nombre";
+
+
+class Usuario {
+    constructor(nombre, mail) {
+        this.nombre = nombre;
+        this.mail = mail;
+    }
+}
+
+const arrayUsuarios = [];
+
+const formulario = document.getElementById("formulario");
+
+formulario.addEventListener("submit", (e)=> {
+    e.preventDefault();
+
+    //Tomamos los datos del formulario:
+    const nombreUsuario = document.getElementById("nombreUsuario");
+    const mailUsuario = document.getElementById("mailUsuario");
+
+    //Creamos el nuevo objeto: 
+    const cliente = new Cliente(nombrecito.value, apellidito.value);
+
+    //Lo metemos al Array:
+    arrayClientes.push(cliente);
+
+    //Verificamos por consola: 
+    console.log(arrayClientes);
+
+    //Reseteamos el formulario: 
+    formularito.reset();
+})
+
+function menu() {
     let elegirOpcion = "";
     while (elegirOpcion !== "5") {
         elegirOpcion = prompt("Selecciona una opción: \n 1) Agregar tarea \n 2) Agregar evento \n 3) Salir");
